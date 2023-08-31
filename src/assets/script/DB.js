e18fbae0677a63d174d404d99bbd5535a42aeff9
Vitaliti Dudar <dudarvitalik1@gmail.com>
2023-08-31 18:04:56 +0300

    Add alt to all img

--- src/assets/script/DB.js
+++ src/assets/script/DB.js
@@ -2,109 +2,113 @@ import axios from 'https://cdn.jsdelivr.net/npm/axios@1.4.0/+esm';
 import { paginationList, elementCreator, taskList } from './script.js';
 import { limit, toggleActivePageClass, setPageQuantity } from './pagination.js';
 export class DataBase {
-    constructor() {}
+	constructor() {}
 
-    // Create <li> elements in given <ul> list from mockApi
-    getAllTasks(taskList) {
-        axios
-            .get('https://64be77ea5ee688b6250c7762.mockapi.io/tasks')
-            .then((res) => {
-                res.data.reverse();
-                taskList.replaceChildren();
-                res.data.forEach((DBelement) => {
-                    elementCreator.createListElement(DBelement, taskList);
-                });
-            })
-            .catch((err) => console.error(err));
-    }
+	// Create <li> elements in given <ul> list from mockApi
+	getAllTasks(taskList) {
+		axios
+			.get('https://64be77ea5ee688b6250c7762.mockapi.io/tasks')
+			.then((res) => {
+				res.data.reverse();
+				taskList.replaceChildren();
+				res.data.forEach((DBelement) => {
+					elementCreator.createListElement(DBelement, taskList);
+				});
+			})
+			.catch((err) => console.error(err));
+	}
 
-    // add task to DB after creating a task
-    async addTaskToDB({ name, completed, createdTime, id }) {
-        await axios
-            .post('https://64be77ea5ee688b6250c7762.mockapi.io/tasks', {
-                name,
-                completed,
-                createdTime,
-                id,
-            })
-            .then(async (res) => {
-                await this.pagination(taskList, 1, limit);
+	// add task to DB after creating a task
+	async addTaskToDB({ name, completed, createdTime, id }) {
+		await axios
+			.post('https://64be77ea5ee688b6250c7762.mockapi.io/tasks', {
+				name,
+				completed,
+				createdTime,
+				id,
+			})
+			.then(async (res) => {
+				await this.pagination(taskList, 1, limit);
 
-                elementCreator.createListElement({ name, completed, createdTime, id }, taskList);
-            })
-            .catch((err) => console.error(err));
-    }
+				elementCreator.createListElement(
+					{ name, completed, createdTime, id },
+					taskList
+				);
+			})
+			.catch((err) => console.error(err));
+	}
 
-    markTaskCompleted(id, boolean) {
-        axios
-            .put(`https://64be77ea5ee688b6250c7762.mockapi.io/tasks/${id}`, {
-                completed: boolean,
-            })
-            .then((res) => {})
-            .catch((err) => console.error(err));
-    }
+	markTaskCompleted(id, boolean) {
+		axios
+			.put(`https://64be77ea5ee688b6250c7762.mockapi.io/tasks/${id}`, {
+				completed: boolean,
+			})
+			.then((res) => {})
+			.catch((err) => console.error(err));
+	}
 
-    async deleteTaskFromDB(id) {
-        await axios
-            .delete(`https://64be77ea5ee688b6250c7762.mockapi.io/tasks/${id}`)
-            .then(() => {})
-            .catch((err) => console.error(err));
-    }
+	async deleteTaskFromDB(id) {
+		await axios
+			.delete(`https://64be77ea5ee688b6250c7762.mockapi.io/tasks/${id}`)
+			.then(() => {})
+			.catch((err) => console.error(err));
+	}
 
-    async getJsonTaskList(url) {
-        const response = await axios.get(url);
-        const data = response.data;
+	async getJsonTaskList(url) {
+		const response = await axios.get(url);
+		const data = response.data;
 
-        return data;
-    }
+		return data;
+	}
 
-    pagination = async (taskList, page, limit) => {
-        const url = new URL('https://64be77ea5ee688b6250c7762.mockapi.io/tasks');
+	pagination = async (taskList, page, limit) => {
+		const url = new URL('https://64be77ea5ee688b6250c7762.mockapi.io/tasks');
 
-        url.searchParams.append('sortBy', 'createdTime');
-        url.searchParams.append('order', 'desc');
-        url.searchParams.append('page', page);
-        url.searchParams.append('limit', limit);
+		url.searchParams.append('sortBy', 'createdTime');
+		url.searchParams.append('order', 'desc');
+		url.searchParams.append('page', page);
+		url.searchParams.append('limit', limit);
 
-        axios
-            .get(url)
-            .then(async (res) => {
-                taskList.replaceChildren();
+		axios
+			.get(url)
+			.then(async (res) => {
+				taskList.replaceChildren();
 
-                res.data.forEach((DBelement) => {
-                    elementCreator.createListElement(DBelement, taskList);
-                });
+				res.data.forEach((DBelement) => {
+					elementCreator.createListElement(DBelement, taskList);
+				});
 
-                paginationList.querySelectorAll('.pagination__list-item').forEach((el) => {
-                    if (el.value != 0) el.remove();
-                });
+				paginationList
+					.querySelectorAll('.pagination__list-item')
+					.forEach((el) => {
+						if (el.value != 0) el.remove();
+					});
 
-                elementCreator.displayPagination(paginationList, await setPageQuantity());
+				elementCreator.displayPagination(paginationList, await setPageQuantity());
 
-                toggleActivePageClass(page);
-            })
-            .catch((err) => console.error(err));
-    };
+				toggleActivePageClass(page);
+			})
+			.catch((err) => console.error(err));
+	};
 
-    async getLastTaskIndex() {
-        try {
-            const allTasks = await this.getJsonTaskList(
-                'https://64be77ea5ee688b6250c7762.mockapi.io/tasks'
-            );
-            const lastTask = allTasks[allTasks.length - 1];
-            return lastTask.id;
-        } catch (error) {
-            return 1;
-            alert('getLastIndex' + error);
-        }
-    }
+	async getLastTaskIndex() {
+		try {
+			const allTasks = await this.getJsonTaskList(
+				'https://64be77ea5ee688b6250c7762.mockapi.io/tasks'
+			);
+			const lastTask = allTasks[allTasks.length - 1];
+			return lastTask.id;
+		} catch (error) {
+			return 1;
+		}
+	}
 
-    async getPagesQuantity(limit) {
-        const allTasks = await this.getJsonTaskList(
-            'https://64be77ea5ee688b6250c7762.mockapi.io/tasks'
-        );
-        const taskQuantity = allTasks.length;
+	async getPagesQuantity(limit) {
+		const allTasks = await this.getJsonTaskList(
+			'https://64be77ea5ee688b6250c7762.mockapi.io/tasks'
+		);
+		const taskQuantity = allTasks.length;
 
-        return Math.ceil(taskQuantity / limit);
-    }
+		return Math.ceil(taskQuantity / limit);
+	}
 }
--- src/assets/script/script.js
+++ src/assets/script/script.js
@@ -2,98 +2,102 @@ import { DataBase } from './DB.js';
 import { createElements } from './createElements.js';
 import { main, limit, toggleActivePageClass, setPageQuantity } from './pagination.js';
 
+/=============== GENERAL TODO APP ELEMENTS ===============/;
 const taskList = document.querySelector('#task-list');
-const inputBox = document.querySelector('#input-box');
 const todayDate = document.querySelector('#today-date');
-
 const paginationList = document.querySelector('#pagination');
 
 const addTaskButton = document.querySelector('.todo__button');
 const addTaskField = document.querySelector('.todo__add');
 
+/=============== ADD TASK INPUT ===============/;
+const inputBox = document.querySelector('#input-box');
 const confirmButton = document.querySelector('.todo__input-confirm');
 const clearButton = document.querySelector('#input-clear-button');
 
+/=============== IMPORTED CLASSES ===============/;
 const db = new DataBase();
 const elementCreator = new createElements();
 
 // event listener for button which create html task elements
 
 async function confirm() {
-    if (inputBox.value == '') {
-        alert('You must write something');
-        return;
-    }
+	if (inputBox.value == '') {
+		alert('You must write something');
+		return;
+	}
 
-    await db.addTaskToDB({
-        name: inputBox.value,
-        completed: false,
-        createdTime: new Date().getTime(),
-        id: db.getLastTaskIndex() + 1,
-    });
+	await db.addTaskToDB({
+		name: inputBox.value,
+		completed: false,
+		createdTime: new Date().getTime(),
+		id: db.getLastTaskIndex() + 1,
+	});
 
-    inputBox.value = '';
+	inputBox.value = '';
 }
 
 confirmButton.addEventListener('click', () => {
-    confirm();
+	confirm();
 });
 
 inputBox.addEventListener('keyup', (event) => {
-    if (event.key === 'Enter') {
-        confirm();
-    }
+	if (event.key === 'Enter') {
+		confirm();
+	}
 });
 
 //Clear all text from input field
 
 clearButton.addEventListener('click', () => {
-    inputBox.value = '';
-    return;
+	inputBox.value = '';
+	return;
 });
 
 //Add animation after clicking on button
 
 addTaskButton.addEventListener('click', () => {
-    addTaskField.classList.toggle('animation');
-    inputBox.focus();
+	addTaskField.classList.toggle('animation');
+	inputBox.focus();
 });
 
 taskList.addEventListener(
-    'click',
-    async (e) => {
-        //animation on delete window
-        let item = e.target.closest('.task__item');
+	'click',
+	async (e) => {
+		//animation on delete window
+		let item = e.target.closest('.task__item');
 
-        if (e.target.closest('.task__delete') === null && e.target.id != 'my-icon') {
-            item.querySelector('.task__delete').classList.toggle('task__delete-animation');
-        } else if (e.target.classList.contains('task__button-icon')) {
-            toggleComplitionOfTask(item);
-        } else if (e.target.closest('.task_delete-button') !== null) {
-            item.remove();
-            await db.deleteTaskFromDB(item.id);
-            db.pagination(
-                taskList,
-                document.querySelector('.pagination__list-item--active').value,
-                limit
-            );
-        }
-    },
-    false
+		if (e.target.closest('.task__delete') === null && e.target.id != 'my-icon') {
+			item.querySelector('.task__delete').classList.toggle(
+				'task__delete-animation'
+			);
+		} else if (e.target.classList.contains('task__button-icon')) {
+			toggleComplitionOfTask(item);
+		} else if (e.target.closest('.task_delete-button') !== null) {
+			item.remove();
+			await db.deleteTaskFromDB(item.id);
+			db.pagination(
+				taskList,
+				document.querySelector('.pagination__list-item--active').value,
+				limit
+			);
+		}
+	},
+	false
 );
 
 //Complete task
 function toggleComplitionOfTask(listItem) {
-    const imgElement = listItem.querySelector('.task__button-icon');
-    listItem.classList.toggle('task__completed');
+	const imgElement = listItem.querySelector('.task__button-icon');
+	listItem.classList.toggle('task__completed');
 
-    if (imgElement.src.includes('uncompleted')) {
-        imgElement.src = './src/components/icons/task__completed-button.svg';
-        db.markTaskCompleted(listItem.id, true);
-        return;
-    }
-    imgElement.src = './src/components/icons/task__uncompleted-button.svg';
-    db.markTaskCompleted(listItem.id, false);
+	if (imgElement.src.includes('uncompleted')) {
+		imgElement.src = './src/components/icons/task__completed-button.svg';
+		db.markTaskCompleted(listItem.id, true);
+		return;
+	}
+	imgElement.src = './src/components/icons/task__uncompleted-button.svg';
+	db.markTaskCompleted(listItem.id, false);
 }
 
 main();
